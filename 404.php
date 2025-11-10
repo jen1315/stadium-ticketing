0e27f3323e93a4093d683a8efafd9dfb1a20e6dc
Jen1315 <47667135+jen1315@users.noreply.github.com>
2025-11-10 23:06:37 +0100

    Aggiunta documentazione latex

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php header("HTTP/1.1 404 Not Found"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagina non trovata</title>
+    </head>
+    <body>
+        <header></header>
+        <footer></footer>
+    </body>
+</html>
